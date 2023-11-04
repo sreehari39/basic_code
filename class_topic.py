f8af8696de48b8c20ac1d0448a03b7b9cdb102bf
sreehari <sreeh3979@gmail.com>
2023-11-04 07:32:27 +0530

    multipul classes inherited

--- class_topic.py
+++ class_topic.py
@@ -1,1 +1,21 @@
+# these is multiple inheritance: inheriting 1 or more class in  particlear class  is callled multiple inheritance
+class First:
+    def add(self, x, y):
+        return x + y
 
+
+class Second:
+    def sub(self, x, y):
+        return x - y
+
+
+# class Third(First, Second):
+class Third(Second, First):
+    def mul(self, x, y):
+        return x * y
+
+
+obj = Third()
+print(obj.sub(20, 30))
+print(obj.add(20, 30))
+print(obj.mul(10, 30))
